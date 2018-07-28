132fb2c7db6afd214423e3e0adb0a9af900964a8
acoolnev <anc4cr@gmail.com>
2018-07-28 02:55:58 -0600

    - Added MDC drawer that is intended for image list. The main container will have a map view.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MdcDrawerModule } from '@angular-mdc/web';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdcDrawerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
