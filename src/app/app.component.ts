3b7b9996c68a952dbdb013fc103c57804da1b085
acoolnev <anc4cr@gmail.com>
2018-08-26 01:20:15 -0600

    - Added input of type file that opens files dialog to select photos.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   images = Array.from(Array(4), (x, i) => i);
   currentImage: number = undefined;
+
+  public onSelectedFiles(event: Event) {
+    console.log((<HTMLInputElement>event.target).files);
+  }
 }
